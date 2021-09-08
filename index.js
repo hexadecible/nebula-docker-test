75935e97af19023ce5df4431fd7bdf3e93a0d170
hexadecible <63071090+hexadecible@users.noreply.github.com>
2021-09-08 13:03:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const DHT = require('@hyperswarm/dht');
 (async () => {
   const keyPair = DHT.keyPair();
 
-  const drive = new Drive(__dirname + "/drive", "170ac7b9bb280312427ea89ea31cb8d485c1e3be2f4eb8609f050b64cab046f4", {
+  const drive = new Drive(__dirname + "/drive", null, {
     keyPair,
     swarmOpts: {
       server: true,
@@ -13,8 +13,6 @@ const DHT = require('@hyperswarm/dht');
   });
 
   await drive.ready();
-
-  await drive.addPeer('8f751f910de4883fd6b9193477b73b06da643390df5f442d6763c837ffcf86cd');
 
   console.log('Diff Key ==> ', drive.diffFeedKey);
   console.log('Public Key ==> ', drive.publicKey);
