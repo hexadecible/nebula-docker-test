f2a5fcbfe0e7845c360361f52823c6082bd1c1a9
hexadecibal <hex@telios.io>
2021-09-09 11:50:12 -0400

    adding ipc

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
+const fs = require('fs');
 const Drive = require('@telios/nebula-drive');
 const DHT = require('@hyperswarm/dht');
+const ipc = require('hyper-ipc')('somesupersecrettopic');
 
 (async () => {
   const keyPair = DHT.keyPair();
-  const peerDiffKey = null; // Add peer diff key to begin sync
 
-  const drive = new Drive(__dirname + "/drive", peerDiffKey, {
+  const drive = new Drive(__dirname + "/client_drive", null, {
     keyPair,
     swarmOpts: {
       server: true,
@@ -15,7 +16,23 @@ const DHT = require('@hyperswarm/dht');
 
   await drive.ready();
 
-  // await drive.addPeer('add peer diff key here');
+  if (!fs.existsSync(__dirname + '/client_drive/Files/test_file.txt')) {
+    const readStream = fs.createReadStream(__dirname + '/test_file.txt');
+    await drive.writeFile('/test_file.txt', readStream);
+  }
+
+  ipc.serve('addPeer', async (query, callback) => {
+    console.log('ADDING PEER...');
+    await drive.addPeer(query.toString());
+
+    ipc.run('addPeer', drive.diffFeedKey, (err, data) => {
+      if (err) return console.error(err);
+
+      console.log('PEER ADDED');
+    });
+
+    callback(null, 'done');
+  });
 
   console.log('Public Key ==> ', drive.publicKey);
   console.log('Diff Key ==> ', drive.diffFeedKey);
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
 const Drive = require('@telios/nebula-drive');
 const DHT = require('@hyperswarm/dht');
+const ipc = require('hyper-ipc')('somesupersecrettopic');
 
 (async () => {
   const keyPair = DHT.keyPair();
-  const peerDiffKey = null; // Add peer diff key to begin sync
+  const peerKey = '28b351530a5030fb8ba8fa8d075451eb81518d2ae095fbe18672eec5d14051c8';
 
-  const drive = new Drive(__dirname + "/drive", null, {
+  const drive = new Drive(__dirname + "/server_drive", peerKey, {
     keyPair,
     swarmOpts: {
       server: true,
@@ -14,12 +14,24 @@ const DHT = require('@hyperswarm/dht');
     }
   });
 
+  drive.on('file-sync', (file) => {
+    console.log('FILE', file);
+  });
+
   await drive.ready();
 
-  const readStream = fs.createReadStream(__dirname + '/test_file.txt');
-  await drive.writeFile('/test_file.txt', readStream);
+  ipc.serve('addPeer', async (query, callback) => {
+    console.log('ADDING PEER...');
+    await drive.addPeer(query.toString());
 
-  //await drive.addPeer(peerDiffKey);
+    callback(null, 'done');
+  });
+
+  ipc.run('addPeer', drive.diffFeedKey, (err, data) => {
+    if (err) return console.error(err);
+
+    console.log('PEER ADDED');
+  });
 
   console.log('Public Key ==> ', drive.publicKey);
   console.log('Diff Key ==> ', drive.diffFeedKey);
